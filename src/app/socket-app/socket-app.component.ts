20eabac0393860c56cec92d349a79ef4c4d0bc96
mo <moshpit2241@gmail.com>
2020-05-26 06:09:17 +0800

    fix url

--- src/app/socket-app/socket-app.component.ts
+++ src/app/socket-app/socket-app.component.ts
@@ -13,7 +13,7 @@ export class SocketAppComponent implements OnInit {
 
   commits: any;
 
-  endpoint = 'http://localhost:8080/commits';
+  endpoint = 'https://fathomless-badlands-40049.herokuapp.com/commits';
 
   constructor(private socket: Socket, private http: HttpClient) {
   }
